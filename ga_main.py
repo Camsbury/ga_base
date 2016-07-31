8ad4aaee51364c5c9e4867efbf828014b798d573
buddha <camsbury7@gmail.com>
2016-07-31 03:21:02 -0400

    ga_base1 init
    Starting work on a simple GA project

---
+++ ga_main.py
@@ -0,0 +1,10 @@
+import random
+import numpy as np
+
+
+def optimize(num_inputs, min_range, max_range, fitness_function, \
+population_size, percent_parents = 0.2, percent_unfit = .05, \
+percent_mutate = 0.01):
+    pass
+
+
