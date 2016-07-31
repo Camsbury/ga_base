9ad58315aa188131e272b4381ab788d90ca64361
buddha <camsbury7@gmail.com>
2016-07-31 03:58:41 -0400

    reformatted

--- ga_base.py
+++ ga_base.py
@@ -1,10 +1,12 @@
 import random
 import numpy as np
-
+from ~/Documents/Projects/python/personal/ga_base1/ga_base \
+import random_individual 
 
 def optimize(num_inputs, min_range, max_range, fitness_function, \
 population_size, percent_parents = 0.2, percent_unfit = .05, \
 percent_mutate = 0.01):
-    pass
+
+    
 
 
